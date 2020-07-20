87b38a340e8544408b17461ec165b826555aef62
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-20 17:02:26 +0100

    Add test for toner

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,18 +1,25 @@
 public class Printer {
     private int sheets;
+    private int toner;
 
-    public Printer(int sheets) {
+    public Printer(int sheets, int toner) {
         this.sheets = sheets;
+        this.toner = toner;
     }
 
     public int getSheets() {
         return this.sheets;
     }
 
+    public int getTonerVolume() {
+        return this.toner;
+    }
+
     public void print(int pages, int copies) {
         int pagesNumber = pages * copies;
-        if(this.sheets >= pagesNumber) {
+        if(this.sheets >= pagesNumber && this.toner >= pagesNumber) {
             this.sheets -= pagesNumber;
+            this.toner -= pagesNumber;
         }
     }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -8,18 +8,25 @@ public class PrinterTest {
 
     @Before
     public void before() {
-        this.printer = new Printer(50);
+        this.printer = new Printer(50, 100);
     }
 
     @Test
     public void canPrint() {
         printer.print(10, 4);
         assertEquals(10, printer.getSheets());
+        assertEquals(60, printer.getTonerVolume());
     }
 
     @Test
-    public void cannotPrint() {
+    public void cannotPrintNoSufficientSheets() {
         printer.print(10, 6);
         assertEquals(50, printer.getSheets());
+    }
+
+    @Test
+    public void cannotPrintNoSufficientToner() {
+        printer.print(15, 10);
+        assertEquals(100, printer.getTonerVolume());
     }
 }
