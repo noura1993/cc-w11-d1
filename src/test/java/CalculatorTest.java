1b379c5f9a54f9866925fee6001595268737258d
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-20 15:06:57 +0100

    Add test for Add function

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void before() {
+        this.calculator = new Calculator();
+    }
+
+    @Test
+    public void add() {
+        assertEquals(8, calculator.add(3, 5));
+    }
+
+}
